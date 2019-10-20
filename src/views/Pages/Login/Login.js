7f822bc5c1cd198295c191679e8674563453b09d
Fabricio Orrala <fabricio_fdop_007@hotmail.com>
2019-10-20 10:47:52 -0500

    Levector base

---
+++ src/views/Pages/Login/Login.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Card, CardBody, CardGroup, Col, Container, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
+
+class Login extends Component {
+  render() {
+    return (
+      <div className="app flex-row align-items-center">
+        <Container>
+          <Row className="justify-content-center">
+            <Col md="8">
+              <CardGroup>
+                <Card className="p-4">
+                  <CardBody>
+                    <Form>
+                      <h1>Login</h1>
+                      <p className="text-muted">Ingresa a tu cuenta</p>
+                      <InputGroup className="mb-3">
+                        <InputGroupAddon addonType="prepend">
+                          <InputGroupText>
+                            <i className="icon-user"></i>
+                          </InputGroupText>
+                        </InputGroupAddon>
+                        <Input type="text" placeholder="Username" autoComplete="username" />
+                      </InputGroup>
+                      <InputGroup className="mb-4">
+                        <InputGroupAddon addonType="prepend">
+                          <InputGroupText>
+                            <i className="icon-lock"></i>
+                          </InputGroupText>
+                        </InputGroupAddon>
+                        <Input type="password" placeholder="Password" autoComplete="current-password" />
+                      </InputGroup>
+                      <Row>
+                        <Col xs="6">
+                          <Button color="dark" className="px-4">Iniciar sesión</Button>
+                        </Col>
+                        <Col xs="6" className="text-right">
+                          <Button color="link" className="px-0">Olvidé mi contraseña</Button>
+                        </Col>
+                      </Row>
+                    </Form>
+                  </CardBody>
+                </Card>
+                <Card className="text-white bg-primary py-5 d-md-down-none" style={{ width: '44%' }}>
+                  <CardBody className="text-center">
+                    <div>
+                      <h2>Levector Admin</h2>
+                      <p>Plataforma para gestión interna de casting, locaciones y utilería dentro de Levector.</p>
+                      {/*
+                      <Link to="/register">
+                        <Button color="primary" className="mt-3" active tabIndex={-1}>Register Now!</Button>
+                      </Link>
+                      */}
+                    </div>
+                  </CardBody>
+                </Card>
+              </CardGroup>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default Login;

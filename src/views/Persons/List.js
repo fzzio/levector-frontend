bb2188a4e3410ee76f04c0a43374ed3e46dab054
Ing. Henry Lomas A <khenryscorpio@gmail.com>
2019-11-24 21:31:11 -0500

    Formulario de registro v1, problemas con api

--- src/views/Persons/List.js
+++ src/views/Persons/List.js
@@ -1,8 +1,138 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Badge, Card, CardBody, CardFooter, CardHeader, Col, Row, Table, Button} from 'reactstrap';
+import {
+  Badge,
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Col,
+  Collapse,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  Fade,
+  Form,
+  FormGroup,
+  FormText,
+  FormFeedback,
+  Input,
+  InputGroup,
+  InputGroupAddon,
+  InputGroupButtonDropdown,
+  InputGroupText,
+  Label,
+  Row,
+} from 'reactstrap';
 import axios from 'axios';
 import defines from '../../defines'
+const people = [
+  "Siri",
+  "Alexa",
+  "Google",
+  "Facebook",
+  "Twitter",
+  "Linkedin",
+  "Sinkedin"
+];
+
+
+function Search(props) {
+  const person = props.person
+  const [firstname, setName] = useState('')
+  const [lastname, setLastname] = useState('')
+  const [skill, setSkill] = useState('')
+  const [weight, setWeight] = useState('')
+  const [height, setHeight] = useState('')
+  const submit = e => {
+    e.preventDefault()
+    fetch(`http://localhost:3000/searchperson/`, {
+      method: 'POST',
+      body: JSON.stringify({ firstname, lastname }),
+      /**aqui van mas campos */
+    })
+  }
+  return (
+
+    <Col xs="12" sm="12" md="12">
+      <form onSubmit={submit}>
+        <Col lg="3" sm="6" md="6">
+          <label htmlFor="firstname">Name</label>
+          <input class="form-control"
+            name="firstname" placeholder="Juan"
+            value={firstname}
+            onChange={e => setName(e.target.value)}
+          />
+        </Col>
+        <Col lg="3" sm="6" md="6">
+          <label htmlFor="lastname">Lastname</label>
+          <input class="form-control"
+            name="lastname" placeholder="Lomas"
+            value={lastname}
+            onChange={e => setLastname(e.target.value)}
+          />
+        </Col>
+        <Col lg="3" sm="6" md="6">
+          <label htmlFor="skill">Aptitudes</label>
+          <input class="form-control" 
+            name="skill" placeholder="Actuar, bailar..."
+            value={skill}
+            onChange={e => setSkill(e.target.value)}
+          />
+        </Col>
+        <Col lg="2" sm="3" md="3">
+          <label htmlFor="weight">Peso</label>
+          <input class="form-control"
+            name="weight" placeholder="x Kg"
+            value={weight}
+            onChange={e => setWeight(e.target.value)}
+          />
+        </Col>
+        <Col lg="2" sm="3" md="3">
+          <label htmlFor="height">Altura</label>
+          <input class="form-control"
+            name="height" placeholder="y Cm"
+            value={height}
+            onChange={e => setHeight(e.target.value)}
+          />
+        </Col>
+          <Col md="3">
+            <Label htmlFor="address">Dirección</Label>
+          </Col>
+          <Col xs="9" md="7">
+            <Input
+              type="textarea"
+              name="address"
+              id="address"
+              rows="3"
+              placeholder="Urdesa Central, Cedros 215 y Victor Emilio Estrada"
+            />
+          </Col>
+        <hr></hr>
+        
+        <Col xs="12" md="6">
+          <Card>
+            <CardHeader>
+              <strong>Otros</strong> Datos adicionales
+                </CardHeader>
+            <CardBody>
+            </CardBody>
+          </Card>
+        </Col>
+        <hr></hr>
+        <Col lg="3" sm="6" md="6">
+        <button type="submit">Send it!</button>
+        </Col>
+      </form>
+     
+    </Col >
+    
+
+
+  )
+}
+
 
 function PersonItem(props) {
   const person = props.person
@@ -18,6 +148,7 @@ function PersonItem(props) {
 
   return (
     <Col xs="12" sm="4" md="3">
+
       <Card id={person.ID}>
         <CardBody>
           <Row>
@@ -41,17 +172,20 @@ function PersonItem(props) {
         </CardFooter>
       </Card>
     </Col>
+
+
+
   )
 }
 
 class List extends Component {
   constructor(props) {
     super(props)
-      this.state = {
-        persons: [],
-        loading: true,
-        error: false,
-      }
+    this.state = {
+      persons: [],
+      loading: true,
+      error: false,
+    }
   }
 
   componentDidMount() {
@@ -60,20 +194,20 @@ class List extends Component {
     axios.get(
       defines.API_DOMAIN + '/person/' + offset + '/' + limit
     )
-    .then( (response) => {
-      if(response.status === 200 ) {
-        this.setState({ persons: response.data.data })
-      }else{
-        throw new Error("Invalid status code");
-      }
-    })
-    .catch( (err) => {
-      console.log(err)
-    });
+      .then((response) => {
+        if (response.status === 200) {
+          this.setState({ persons: response.data.data })
+        } else {
+          throw new Error("Invalid status code");
+        }
+      })
+      .catch((err) => {
+        console.log(err)
+      });
   }
 
   render() {
-    const personList = this.state.persons 
+    const personList = this.state.persons
 
     return (
       <div className="animated fadeIn">
@@ -85,8 +219,11 @@ class List extends Component {
               </CardHeader>
               <CardBody>
                 <Row>
+                  <Search />
+                </Row>
+                <Row>
                   {personList.map((person, index) =>
-                    <PersonItem key={index} person={person}/>
+                    <PersonItem key={index} person={person} />
                   )}
                 </Row>
               </CardBody>

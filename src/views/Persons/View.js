45c37ffd4f0e706c873102830eb23cd4e7cb46c4
Fabricio Orrala <fabricio_fdop_007@hotmail.com>
2020-02-17 18:57:34 -0500

    full screen

--- src/views/Persons/View.js
+++ src/views/Persons/View.js
@@ -67,6 +67,7 @@ class View extends Component {
                     return {
                         original: defines.API_DOMAIN + defines.PERSON_PATH_IMG_OPTIMIZED + photo.optimized,
                         thumbnail: defines.API_DOMAIN + defines.PERSON_PATH_IMG_THUMBNAIL + photo.thumbnail,
+                        fullscreen: defines.API_DOMAIN + defines.PERSON_PATH_IMG_ORIGINAL + photo.url,
                         originalAlt: personData.dni,
                         thumbnailAlt: personData.dni,
                     }
